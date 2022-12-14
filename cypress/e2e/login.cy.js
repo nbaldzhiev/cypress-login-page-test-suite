22795ed567201e8c88b250b885b18e454163e57d
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2022-12-14 02:33:15 +0200

    initial run of prettier

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,8 @@
-import LoginPage from '../pages/LoginPage'
+import LoginPage from "../pages/LoginPage";
 
-describe('Log In Page', () => {
-
-  const gitHubName = 'GitHub';
-  const googleName = 'Google';
+describe("Log In Page", () => {
+  const gitHubName = "GitHub";
+  const googleName = "Google";
 
   beforeEach(() => {
     // Clear the cookies and open the log in page before each test
@@ -12,40 +11,42 @@ describe('Log In Page', () => {
     loginPage.visit();
   });
 
-  it('All Expected Elements Are Visible', () => {
+  it("All Expected Elements Are Visible", () => {
     // Verifies that all expected elements are visible
     const loginPage = new LoginPage();
 
     loginPage.assertThat.allPageElementsAreVisible();
   });
 
-  it('Non-existent Users Cannot Log In via Email', () => {
+  it("Non-existent Users Cannot Log In via Email", () => {
     // Verifies that a non-existent user cannot log in via email
     const logInPage = new LoginPage();
 
-    logInPage.openLogInWithEmail().logIn(`i-definitely-${Date.now()}@dont.exist`, Date.now());
+    logInPage
+      .openLogInWithEmail()
+      .logIn(`i-definitely-${Date.now()}@dont.exist`, Date.now());
     logInPage.assertThat.logInFormErrorMessageExists();
   });
 
-  [{[gitHubName]: 'https://github.com/'}, {[googleName]: 'https://accounts.google.com/'}].forEach(
-    (authProvider) => {
-      it(`Opening the Log In With ${Object.keys(authProvider)[0]} Page`, () => {
-        // A parameterised test to verify that clicking on the Log in with <auth provider> button opens the 
-        // corresponding page. Currently, the newly opened page is only verified through the URL
-        const loginPage = new LoginPage();
+  [
+    { [gitHubName]: "https://github.com/" },
+    { [googleName]: "https://accounts.google.com/" },
+  ].forEach((authProvider) => {
+    it(`Opening the Log In With ${Object.keys(authProvider)[0]} Page`, () => {
+      // A parameterised test to verify that clicking on the Log in with <auth provider> button opens the
+      // corresponding page. Currently, the newly opened page is only verified through the URL
+      const loginPage = new LoginPage();
 
-        if (Object.keys(authProvider)[0] === gitHubName) {
-          loginPage.openLogInWithGitHub();
-        } else if (Object.keys(authProvider)[0] === googleName) {
-          loginPage.openLogInWithGoogle();
-        }
+      if (Object.keys(authProvider)[0] === gitHubName) {
+        loginPage.openLogInWithGitHub();
+      } else if (Object.keys(authProvider)[0] === googleName) {
+        loginPage.openLogInWithGoogle();
+      }
 
-        // on('url:changed') is necessary because cy.url() does not capture the new URL for some reason
-        cy.on('url:changed', (newUrl) => {
-          expect(newUrl).to.contain(Object.values(authProvider)[0]);
-        });
-      })
-    }
-  );
-
-})
+      // on('url:changed') is necessary because cy.url() does not capture the new URL for some reason
+      cy.on("url:changed", (newUrl) => {
+        expect(newUrl).to.contain(Object.values(authProvider)[0]);
+      });
+    });
+  });
+});
--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -1,120 +1,116 @@
-const LoginPageURL = 'https://cloud.cypress.io/login';
+const LoginPageURL = "https://cloud.cypress.io/login";
 
 class LogInEmailForm {
+  get emailInput() {
+    // Gets and returns the Email input element
+    return cy.get("input#email");
+  }
 
-    get emailInput() {
-        // Gets and returns the Email input element
-        return cy.get('input#email');
-    }
+  get passwordInput() {
+    // Gets and returns the Password input element
+    return cy.get("input#password");
+  }
 
-    get passwordInput() {
-        // Gets and returns the Password input element
-        return cy.get('input#password');
-    }
+  get submitButton() {
+    // Gets and returns the Submit button
+    return cy.get('button[type="submit"]');
+  }
 
-    get submitButton() {
-        // Gets and returns the Submit button
-        return cy.get('button[type="submit"]');
-    }
+  getErrorMessage(foundTimeout = 10000) {
+    // Gets and returns the error message present in case of an error
+    // Overrides the timeout for finding the element with 10 seconds
+    return cy.get('div[class="error-message"]', { timeout: foundTimeout });
+  }
 
-    getErrorMessage(foundTimeout = 10000) {
-        // Gets and returns the error message present in case of an error
-        // Overrides the timeout for finding the element with 10 seconds
-        return cy.get('div[class="error-message"]', {timeout: foundTimeout})
-    }
-
-    logIn(email, password) {
-        // Fills in the form by using the parameter values and submits
-        this.emailInput.type(email);
-        this.passwordInput.type(password);
-        this.submitButton.click();
-    }
-
+  logIn(email, password) {
+    // Fills in the form by using the parameter values and submits
+    this.emailInput.type(email);
+    this.passwordInput.type(password);
+    this.submitButton.click();
+  }
 }
 
 class LoginPage {
+  constructor(pageUrl = LoginPageURL) {
+    this.pageUrl = pageUrl;
+  }
 
-    constructor(pageUrl = LoginPageURL) {
-        this.pageUrl = pageUrl;
+  visit() {
+    // Opens the Login page if not already open
+    if (cy.url() !== LoginPageURL) {
+      cy.visit(this.pageUrl);
     }
+  }
 
-    visit() {
-        // Opens the Login page if not already open
-        if (cy.url() !== LoginPageURL) {
-            cy.visit(this.pageUrl);
-        }
-    }
+  get logInWithGithubButton() {
+    // Gets and returns the "Log in with GitHub" button
+    return cy.get('button[class*="provider-github"]');
+  }
 
-    get logInWithGithubButton() {
-        // Gets and returns the "Log in with GitHub" button
-        return cy.get('button[class*="provider-github"]');
-    }
-    
-    get logInWithGoogleButton() {
-        // Gets and returns the "Log in with Google" button
-        return cy.get('button[class*="provider-google"]');
-    }
+  get logInWithGoogleButton() {
+    // Gets and returns the "Log in with Google" button
+    return cy.get('button[class*="provider-google"]');
+  }
 
-    get logInWithSSOButton() {
-        // Gets and returns the "Log in with SSO" button
-        return cy.get('button[class*="provider-sso"]');
-    }
+  get logInWithSSOButton() {
+    // Gets and returns the "Log in with SSO" button
+    return cy.get('button[class*="provider-sso"]');
+  }
 
-    get logInWithEmailButton() {
-        // Gets and returns the "Log in with Email" button
-        return cy.get('button[class*="provider-email"]');
-    }
+  get logInWithEmailButton() {
+    // Gets and returns the "Log in with Email" button
+    return cy.get('button[class*="provider-email"]');
+  }
 
-    get signUpLink() {
-        // Gets and returns the "Sign up" link
-        return cy.get('a[href="/signup"]');
-    }
+  get signUpLink() {
+    // Gets and returns the "Sign up" link
+    return cy.get('a[href="/signup"]');
+  }
 
-    get logInForm() {
-        return new LogInEmailForm();
-    }
+  get logInForm() {
+    return new LogInEmailForm();
+  }
 
-    openLogInWithEmail() {
-        // Clicks on the "Log in with email" button and returns the form as an LogInEmailForm object
-        this.visit();
-        this.logInWithEmailButton.click();
-        return this.logInForm;
-    }
+  openLogInWithEmail() {
+    // Clicks on the "Log in with email" button and returns the form as an LogInEmailForm object
+    this.visit();
+    this.logInWithEmailButton.click();
+    return this.logInForm;
+  }
 
-    openLogInWithGitHub() {
-        // Clicks on the Log in with GitHub button to open the page
-        this.logInWithGithubButton.click()
-    }
+  openLogInWithGitHub() {
+    // Clicks on the Log in with GitHub button to open the page
+    this.logInWithGithubButton.click();
+  }
 
-    openLogInWithGoogle() {
-        // Clicks on the Log in with Google button to open the page
-        this.logInWithGoogleButton.click()
-    }
+  openLogInWithGoogle() {
+    // Clicks on the Log in with Google button to open the page
+    this.logInWithGoogleButton.click();
+  }
 
-    get assertThat() {
-        return new LoginPageAssertions(this);
-    }
-
+  get assertThat() {
+    return new LoginPageAssertions(this);
+  }
 }
 
 class LoginPageAssertions {
+  constructor(logInPageObj) {
+    this.logInPageObj = logInPageObj;
+  }
 
-    constructor(logInPageObj) {
-        this.logInPageObj = logInPageObj;
-    }
+  allPageElementsAreVisible() {
+    this.logInPageObj.logInWithGithubButton.should("be.visible");
+    this.logInPageObj.logInWithGoogleButton.should("be.visible");
+    this.logInPageObj.logInWithSSOButton.should("be.visible");
+    this.logInPageObj.logInWithEmailButton.should("be.visible");
+    this.logInPageObj.signUpLink.should("be.visible");
+  }
 
-    allPageElementsAreVisible() {
-        this.logInPageObj.logInWithGithubButton.should('be.visible');
-        this.logInPageObj.logInWithGoogleButton.should('be.visible');
-        this.logInPageObj.logInWithSSOButton.should('be.visible');
-        this.logInPageObj.logInWithEmailButton.should('be.visible');
-        this.logInPageObj.signUpLink.should('be.visible');
-    }
-
-    logInFormErrorMessageExists() {
-        this.logInPageObj.logInForm.getErrorMessage().should('contain', 'Incorrect email address or password');
-    }
-
+  logInFormErrorMessageExists() {
+    this.logInPageObj.logInForm
+      .getErrorMessage()
+      .should("contain", "Incorrect email address or password");
+  }
 }
 
 export default LoginPage;
